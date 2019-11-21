1b5819879eb7166028e66e19dc1884f1a36a71ef
Bastian Kuelzer <bastiankuelzer@gmx.de>
2019-11-21 10:16:41 +0100

    Updates

---
+++ precache-manifest.84091dd4a6f65f214e9486ce64770236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7796446cc4c4af779dca82faf921999b",
+    "url": "/Startup_YP_Friend/index.html"
+  },
+  {
+    "revision": "e424e236a12b4339a53f",
+    "url": "/Startup_YP_Friend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "275a9cd351e9e7e11e3f",
+    "url": "/Startup_YP_Friend/static/js/2.3d9806eb.chunk.js"
+  },
+  {
+    "revision": "e424e236a12b4339a53f",
+    "url": "/Startup_YP_Friend/static/js/main.6f3dbe55.chunk.js"
+  },
+  {
+    "revision": "d1cc5c5ca4a856d0ee65",
+    "url": "/Startup_YP_Friend/static/js/runtime-main.fdfe9a65.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Startup_YP_Friend/static/media/logo.25bf045c.svg"
+  }
+]);
